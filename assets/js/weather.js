213b6daaacdf3fa0b47fb01777e0f06d8311eaad
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-06-24 23:44:30 -0700

    current weather properly displayed

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,7 +1,9 @@
 var apiKey = '04ca4740a12eec5578603b1b53763759';
+var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#city-name');
-var currentWeatherContainerEl = document.querySelector('#current-weather-container');
+var citiesListEl = document.querySelector('#cities-container ul');
 var citySearchedEl = document.querySelector('#city-searched');
+var currentWeatherContainerEl = document.querySelector('#current-weather-container');
 var cities = [];
 
 // utility function to check is an object is empty
@@ -13,61 +15,73 @@ var isEmpty = function(obj) {
     return true;
 }
 
+// utility function to capitalize the letter of each word in a string
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1).toLowerCase();     
+    }
+    // directly return the joined string
+    return splitStr.join(' '); 
+ }
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var city = titleCase(cityInputEl.value.trim());
+
+    if (city) {
+        getCurrentWeather(city);
+        cityInputEl.value = '';
+    } else {
+        alert('Please enter a city');
+    }
+};
+
 var getCurrentWeather = function(cityName) {
     // format the OpenWeather api url for current weather, one location
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
 
     // make a request to the url
-    fetch(apiURL).then(function(response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayCurrentWeather(data, cityName);
-            });
-        } else {
-            alert('Error: ' + response.statusText);
-        }
+    fetch(apiURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        displayCurrentWeather(data, cityName);
+        return data.coord;
+    })
+    .then(function(coord) {
+        getUVindex(coord.lat, coord.lon);
     })
     .catch(function(error) {
-        alert('Unable to connect to OpenWeather');
+        alert('Unable to find weather conditions for this city');
     });
 };
-
-var getUVindex = function(lat, lon) {
-    var indexValue;
-    // format the OpenWeather api url for current UV index, one location
-    var apiURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
-    fetch(apiURL).then(function(response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-                indexValue = data.value;
-                console.log(indexValue);
-            });
-        } else {
-            indexValue = 'Error fetching the UV index: ' + response.statusText;
-        }
-    })
-    .catch(function(error) {
-        indexValue = 'Unable to connect to OpenWeather for UV data';
-    });
-    console.log(indexValue);
-    return indexValue;
-}
 
 var displayCurrentWeather = function(data, cityName) {
     // clear old content
     currentWeatherContainerEl.textContent = '';
     citySearchedEl.textContent = cityName;
 
+    // get today's date
+    var today = moment().format('MM/DD/YY');
+
     // display city name in search history if not already there
-    console.log(cityName);
+    if (!cities.includes(cityName)) {
+        searchHistory(cityName);
+    }
 
     // check if api returned an empty weather data object
     if (isEmpty(data)) {
         currentWeatherContainerEl.textContent = 'No weather data found for this city.';
         return;
     }
+
+    // extract weather icon & display title including city, today's date and weather icon
+    var iconId = data.weather[0].icon;
+    citySearchedEl.innerHTML = `${cityName} (${today}) <span id="weather-icon"><img src="http://openweathermap.org/img/wn/${iconId}@2x.png"/></span>`;
 
     // extract & display temperature from the data object
     var temperatureEl = document.createElement('p');
@@ -83,29 +97,48 @@ var displayCurrentWeather = function(data, cityName) {
     var windSpeedEl = document.createElement('p');
     windSpeedEl.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
     currentWeatherContainerEl.appendChild(windSpeedEl);
+}
 
-    // extract latitude and longitude from the data object to fetch for UV index
-    var cityLat = data.coord.lat;
-    var cityLon = data.coord.lon;
-    var UVindex = getUVindex(cityLat, cityLon);
-    console.log('UVindex', UVindex);
-    // display UV index with different class depending on UV index value
-    var UVindexEl = document.createElement('p');
-    if (UVindex.split()[0] === 'E') {
-        UVindexEl.textContent = UVindex;
+var getUVindex = function(lat, lon) {
+    // format the OpenWeather api url for current UV index, one location
+    var apiURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    fetch(apiURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var index = parseFloat(data.value)
+        displayUVindex(index);
+    })
+    .catch(function(error) {
+        alert(`Error fetching the UV index: ${response.statusText}`);
+    });
+}
+
+// Function to display UV index with different class depending on UV index value
+var displayUVindex = function(index) {
+    var indexId;
+    if (index < 3) {
+        indexId = 'uv-fav';
     }
-    else if (parseFloat(UVindex) < 3) {
-        UVindexEl.innerHTML = `UV index: <span id="uv-fav">${UVindex}</span>`;
-    }
-    else if (parseFloat(UVindex) < 6) {
-        UVindexEl.innerHTML = `UV index: <span id="uv-mod">${UVindex}</span>`;
+    else if (index < 6) {
+        indexId = 'uv-mod';
     }
     else {
-        UVindexEl.innerHTML = `UV index: <span id="uv-bad">${UVindex}</span>`;
+        indexId = 'uv-bad';
     }
+    var UVindexEl = document.createElement('p');
+    UVindexEl.innerHTML = `UV index: <span id=${indexId}>${index}</span>`;
     currentWeatherContainerEl.appendChild(UVindexEl);
 }
 
 // Function to populate the search history and save to local storage
+var searchHistory = function(cityName) {
+    cities.push(cityName);
+    cities.sort().forEach(function (){
+        var cityEl = document.createElement('li').addClass('list-item');
+        citiesListEl.appendChild(cityEl);
+    });
+};
 
-getCurrentWeather('Los Angeles');
+userFormEl.addEventListener('submit', formSubmitHandler);
