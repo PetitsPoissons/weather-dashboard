e34249c4b72d0e1c85d1cbff028bdd1dabc117eb
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-06-28 09:00:10 -0700

    cleaned up comments in code

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -36,7 +36,7 @@ var formSubmitHandler = function(event) {
     event.preventDefault();
     // get value from input element
     var city = titleCase(cityInputEl.value.trim());
-
+    // check a value was entered
     if (city) {
         getCurrentWeather(city);
         cityInputEl.value = '';
@@ -45,6 +45,7 @@ var formSubmitHandler = function(event) {
     }
 };
 
+// function to fetch current day/time, weather conditions, latitude and longitude for city searched
 var getCurrentWeather = function(cityName) {
     // format the OpenWeather api url for current weather, one location
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
@@ -52,31 +53,28 @@ var getCurrentWeather = function(cityName) {
     fetch(apiURL)
     .then(response => response.json())
     .then(data => {
-        displayCurrentWeather(data, cityName);
-        return data.coord;
+        displayCurrentWeather(data, cityName);  // call function to display current day/time and weather conditions
+        return data.coord;                      // extract city coordinates object and pass it on to additional functions below
     })
     .then(coord => {
-        getUVindex(coord.lat, coord.lon);
-        get5dayForecast(coord.lat, coord.lon);
+        getUVindex(coord.lat, coord.lon);       // call function to fetch UV index based on city coordinates
+        get5dayForecast(coord.lat, coord.lon);  // call function to fetch 5-day forecast based on city coordinates
     })
-    .catch(error => {
-        alert('Uh-ho, something went wrong... Please check for any misspelling. There could also be a problem with the connection, or there is no weather data available for this city.');
+    .catch(error => {                           // error handling - if fetch is not successful we don't want the input entered to remain in the search history
+        alert('Something went wrong... Please check for any misspelling. There could also be a problem with the connection, or there is no weather data available for this city.');
         forecastContainerEl.innerHTML = '';
-        console.log('alert cities before:', cities);
-        if (cities.includes(cityName)) {
+        if (cities.includes(cityName)) {        // unsuccessful search was stored in cities array, then remove it
             var index = cities.indexOf(cityName);
             if (index > -1) {
                 cities.splice(index, 1);
             }
-            //cities.sort();              // sort alphabetically
             localStorage.setItem('citiesSearched', JSON.stringify(cities)); // save updated array 
-            searchHistory();            // display updated search history
+            searchHistory();                                                // display updated search history
         }
-        console.log('alert cities after:', cities);
-
     });
 };
 
+// function to display current weather conditions
 var displayCurrentWeather = (data, cityName) => {
     // clear old content
     currentWeatherContainerEl.innerHTML = '';
@@ -96,13 +94,12 @@ var displayCurrentWeather = (data, cityName) => {
         return;
     }
 
-    // calculate the searched city local day and time
+    // calculate the searched city local day and time given their time zone
     dtCitySearched = moment.unix(data.dt + data.timezone).utc().format('M/DD/YY, h:mm a');
 
     // extract weather icon & display title including city, today's date and weather icon
     var iconId = data.weather[0].icon;
-    console.log('current weather icon:', iconId);
-    citySearchedEl.innerHTML = `${cityName} (${dtCitySearched}) <span id="weather-icon"><img src="http://openweathermap.org/img/wn/${iconId}@2x.png"/></span>`;
+    citySearchedEl.innerHTML = `${cityName} (${dtCitySearched}) <span id="weather-icon"><img src="https://openweathermap.org/img/wn/${iconId}@2x.png"/></span>`;
 
     // extract & display temperature from the data object
     var temperatureEl = document.createElement('p');
@@ -120,36 +117,38 @@ var displayCurrentWeather = (data, cityName) => {
     currentWeatherContainerEl.appendChild(windSpeedEl);
 }
 
+// function to fetch UV index for city searched, based on its latitude and longitude
 var getUVindex = (lat, lon) => {
     // format the OpenWeather api url for current UV index, one location
     var apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
     fetch(apiURL)
     .then(response => response.json())
     .then(data => {
-        var index = parseFloat(data.value)
+        var index = parseFloat(data.value);
         displayUVindex(index);
     })
     .catch(error => alert('Error fetching the UV index'));
 }
 
-// Function to display UV index with different class depending on UV index value
+// function to display UV index with different class depending on UV index value
 var displayUVindex = index => {
     var indexClass;
     if (index < 3) {
-        indexClass = 'bg-success';
+        indexClass = 'bg-success';      // green background if UV index < 3
     }
     else if (index < 6) {
-        indexClass = 'bg-warning';
+        indexClass = 'bg-warning';      // yellow background if UV index < 6
     }
     else {
-        indexClass = 'bg-danger';
+        indexClass = 'bg-danger';       // red background if UV index >= 6
     }
+    // display UV index with appropriate background
     var UVindexEl = document.createElement('p');
     UVindexEl.innerHTML = `UV index: <span class="${indexClass} p-2 text-white rounded">${index}</span>`;
     currentWeatherContainerEl.appendChild(UVindexEl);
 };
 
-// Function to fetch 5-day forecast for a given city
+// function to fetch 5-day forecast for city searched, based on its latitude and longitude
 var get5dayForecast = (lat, lon) => {
     // format the OpenWeather api url for 5-day forecast, one location
     var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
@@ -160,44 +159,38 @@ var get5dayForecast = (lat, lon) => {
     .catch(error => alert('Error fetching the 5-day forecast for this city'));
 };
 
-// Function to display the 5-day forecast for a given city
+// function to display the 5-day forecast, noon each day, calculated based on local day/time of the city searched
 var display5dayForecast = data => {
-    console.log(data);
     // prepare the forecast container
     forecastContainerEl.innerHTML = '<h4 class="d-block pt-4 pb-2">5-Day Forecast <span id="time-forecast">[expected weather conditions at noon each day]</span></h4>';
     var cardsContainerEl = document.createElement('div');
     cardsContainerEl.setAttribute('class', 'row');
 
     // set a date string for first forecast date at noon with format 'YYYY-MM-DD 12:00:00'
-    console.log('dtCitySearched: ', dtCitySearched);
     var firstForecast;
     var todayStartOfHour = moment(dtCitySearched, 'M/DD/YY, h:mm a').startOf('hour').format('YYYY-MM-DD HH:mm:ss');
-    console.log('todayStartOfHour:', todayStartOfHour);
     var todayNineAM = moment(dtCitySearched, 'M/DD/YY, h:mm a').format('YYYY-MM-DD') + ' 09:00:00';
-    console.log('todayNineAM:', todayNineAM);
-    if (todayStartOfHour > todayNineAM) {   // check if current time if more than 9 am, then our first forecast will be next day at noon
+    if (todayStartOfHour > todayNineAM) {   // check if current local time of city searched is after 9 am, then our first forecast will be next day at noon
         firstForecast = moment(dtCitySearched, 'M/DD/YY, h:mm a').add(1, 'd').format('YYYY-MM-DD') + ' 12:00:00';
     } else {                                // otherwise our first forecast will be same day at noon
         firstForecast = moment(dtCitySearched, 'M/DD/YY, h:mm a').format('YYYY-MM-DD') + ' 12:00:00';
     }
-    console.log('firstForecast:', firstForecast);
+
+    // extract array of 40 forecasts (per API doc forecasts are every 3 hours, 5 days, hence 40 data points)
     var arrDays = data.list;
     var startIndex;
-    // get the index for first forecast day in the arrays of d
+    // get the index for first forecast day we identified we wanted
     arrDays.forEach( day => {
         if (day.dt_txt === firstForecast) {
             startIndex = arrDays.indexOf(day);
             return;
         }
     });
-    console.log('startIndex:', startIndex);
 
-    // loop through array of forecasts and create each of the five days forecast (the forecast at 9am is extracted)
+    // loop through array of forecasts and create each of the five forecasts (the forecast at noon is extracted)
     for (i=startIndex; i<arrDays.length; i+=8) {
-        console.log(arrDays[i]);
         var dayForecastContainerEl = document.createElement('div');
         dayForecastContainerEl.setAttribute('class', 'mx-auto');
-        //dayForecastContainerEl.classList = 'col-12';
         var cardEl = document.createElement('div');
         cardEl.setAttribute('class', 'card bg-primary text-white');
         var cardBodyEl = document.createElement('div');
@@ -210,15 +203,16 @@ var display5dayForecast = data => {
         dateEl.textContent = `${date}`;
         cardBodyEl.appendChild(dateEl);
 
-        // extract & display forecast icon
+        /* extract & display forecast icon
+        NOTE: There seems to be a mistake with the API as night icons may be returned even though they are associated with day forecasts.
+        I checked with our TA and he suggested that I dynamically change the last character of the icon to make sure it's a day icon,
+        since I want to display the noon forecasts. */
         var iconId = arrDays[i].weather[0].icon;
-        console.log('forecastIconId:', iconId);
-        if (iconId[iconId.length-1] === 'n') {
-            iconId = iconId.slice(0, -1) + 'd';
-            console.log(iconId);
+        if (iconId[iconId.length-1] === 'n') {          // if it's a night icon, then
+            iconId = iconId.slice(0, -1) + 'd';         // change it to a day icon
         }
         var iconEl = document.createElement('i');
-        iconEl.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconId}.png"/>`;
+        iconEl.innerHTML = `<img src="https://openweathermap.org/img/wn/${iconId}.png"/>`;
         cardBodyEl.appendChild(iconEl);
 
         // extract & display forecasted temp
@@ -233,6 +227,7 @@ var display5dayForecast = data => {
         humidityEl.textContent = `Humidity: ${arrDays[i].main.humidity} %`;
         cardBodyEl.appendChild(humidityEl);
 
+        // append all elements where needed
         cardEl.appendChild(cardBodyEl);
         dayForecastContainerEl.appendChild(cardEl);
         cardsContainerEl.appendChild(dayForecastContainerEl);
@@ -241,7 +236,7 @@ var display5dayForecast = data => {
     forecastContainerEl.appendChild(cardsContainerEl);
 }
 
-// Function to populate the search history and save to local storage
+// function to populate the search history and save to local storage
 var searchHistory = () => {
     // clear previous search history
     citiesListEl.innerHTML = '';
@@ -255,11 +250,13 @@ var searchHistory = () => {
     });
 };
 
+// function to handle click on city displayed in the search history
 var cityClickHandler = event => {
     var cityName = event.target.textContent;
     getCurrentWeather(cityName);
 }
 
+// function to clear the search history
 var clearSearchHistory = () => {
     cities = [];
     localStorage.setItem('citiesSearched', JSON.stringify(cities));
